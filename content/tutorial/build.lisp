379b16e42934a3d3fabbf64ffcefbfe5d6b960aa
vindarel <vindarel@mailz.org>
2025-08-28 12:34:00 +0200

    (minor) build.lisp with uiop:dump-image

--- content/tutorial/build.lisp
+++ content/tutorial/build.lisp
@@ -2,7 +2,14 @@
 
 (ql:quickload "myproject")
 
-(sb-ext:save-lisp-and-die "myproject"
-                          :executable t
-                          :toplevel #'myproject::main
-                          :compression 9)
+(setf uiop:*image-entry-point* #'myproject::main)
+
+(uiop:dump-image "myproject" :executable t :compression 9)
+
+#|
+;; The same as:
+
+(sb-ext:save-lisp-and-die "myproject" :executable t :compression 9
+                          :toplevel #'myproject::main)
+
+|#
