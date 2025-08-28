1d969b1dde0239f43dec2a855fe3aa9980bbad49
vindarel <vindarel@mailz.org>
2025-08-28 12:35:14 +0200

    build

--- docs/tutorial/build.lisp
+++ docs/tutorial/build.lisp
@@ -4,8 +4,12 @@
 
 (setf uiop:*image-entry-point* #'myproject::main)
 
-(uiop:dump-image "myproject"
-                          :executable t
-                          ;; :toplevel #'myproject::main
-                          ;; :entry-point  #'myproject::main
-                          :compression 9)
+(uiop:dump-image "myproject" :executable t :compression 9)
+
+#|
+;; The same as:
+
+(sb-ext:save-lisp-and-die "myproject" :executable t :compression 9
+                          :toplevel #'myproject::main)
+
+|#
